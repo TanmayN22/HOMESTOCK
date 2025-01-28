d09e106698769fe9657734b4e2248eb7b9d3e323
Tanmay Nayak <125031648+TanmayN22@users.noreply.github.com>
2025-01-28 22:15:45 +0530

    sizes

--- home_stock/lib/widgets/appbar.dart
+++ home_stock/lib/widgets/appbar.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:home_stock/utils/constants/sizes.dart';
 
-class Appbar extends StatelessWidget implements PreferredSizeWidget{
+class Appbar extends StatelessWidget implements PreferredSizeWidget {
   const Appbar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
-  
+
   @override
-  // TODO: implement preferredSize
-  Size get preferredSize => throw UnimplementedError();
+  Size get preferredSize => const Size.fromHeight(Sizes.appBarHeight);
 }
